314486dec01a3e8234535487c3a70d101fbce591
Martin Kearn <martin.kearn@microsoft.com>
2018-07-04 09:49:15 +0100

    Initial project

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild AspNetCore-EchoBot-With-State.sln -p:DeployOnBuild=true -p:PublishProfile=botv4luisplay-Web-Deploy.pubxml -p:Password=puGWoo0crEHb4yr9ZdH3luJuy9b1xK8QRvxvPWXJHzHC5JJexuxnkKxW5H2t
+
