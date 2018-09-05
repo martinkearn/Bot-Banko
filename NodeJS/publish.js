f287b935e5ab242b7406dcc1c08a5a45f0c48f21
Martin Kearn <martin.kearn@microsoft.com>
2018-09-05 09:56:41 +0100

    Added default node template from Azure Bot Service

---
+++ NodeJS/publish.js
@@ -0,0 +1,52 @@
+var zipFolder = require('zip-folder');
+var path = require('path');
+var fs = require('fs');
+var request = require('request');
+
+var rootFolder = path.resolve('.');
+var zipPath = path.resolve(rootFolder, '../bankojs.zip');
+var kuduApi = 'https://bankojs.scm.azurewebsites.net/api/zip/site/wwwroot';
+var userName = '$bankojs';
+var password = 'fd9Z4pfCHgquWCkdYgmn42Keq6m2L3FrJwzmDmpSeW9m27tGX6gi00mFRBPa';
+
+function uploadZip(callback) {
+  fs.createReadStream(zipPath).pipe(request.put(kuduApi, {
+    auth: {
+      username: userName,
+      password: password,
+      sendImmediately: true
+    },
+    headers: {
+      "Content-Type": "applicaton/zip"
+    }
+  }))
+  .on('response', function(resp){
+    if (resp.statusCode >= 200 && resp.statusCode < 300) {
+      fs.unlink(zipPath);
+      callback(null);
+    } else if (resp.statusCode >= 400) {
+      callback(resp);
+    }
+  })
+  .on('error', function(err) {
+    callback(err)
+  });
+}
+
+function publish(callback) {
+  zipFolder(rootFolder, zipPath, function(err) {
+    if (!err) {
+      uploadZip(callback);
+    } else {
+      callback(err);
+    }
+  })
+}
+
+publish(function(err) {
+  if (!err) {
+    console.log('bankojs publish');
+  } else {
+    console.error('failed to publish bankojs', err);
+  }
+});
