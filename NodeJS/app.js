ad71970a06a848b288d601973ba32b7f8f10e7db
Martin Kearn <martin.kearn@microsoft.com>
2018-09-05 13:20:26 +0100

    Basic Luis Dialogs

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -4,11 +4,12 @@
 const { BotStateSet, BotFrameworkAdapter, MemoryStorage, ConversationState, UserState } = require('botbuilder');
 const restify = require('restify');
 const { LuisRecognizer } = require('botbuilder-ai');
+const { DialogSet } = require('botbuilder-dialogs');
 require('dotenv').config();
 
 const luisRecognizer = new LuisRecognizer({
-    appId: process.env("LuisModel"),
-    subscriptionKey: process.env("LuisSubscriptionKey"),
+    appId: process.env.LuisModel,
+    subscriptionKey: process.env.LuisSubscriptionKey,
     serviceEndpoint: 'https://westeurope.api.cognitive.microsoft.com'
 });
 
@@ -25,19 +26,35 @@ const adapter = new BotFrameworkAdapter({
 });
 
 // Add state middleware
-const storage = new MemoryStorage();
-const convoState = new ConversationState(storage);
-const userState = new UserState(storage);
-adapter.use(new BotStateSet(convoState, userState));
+const conversationState = new ConversationState(new MemoryStorage());
+adapter.use(conversationState);
 
 // Add the recognizer to your bot
 adapter.use(luisRecognizer);
+
+// register some dialogs for usage with the intents detected by the LUIS app
+const dialogs = new DialogSet();
+
+dialogs.add('BalanceDialog', [
+    async (dialogContext) => {
+        await dialogContext.context.sendActivity(`Your balance is £20.`);
+        await dialogContext.end();
+    }
+]);
+
+dialogs.add('TransferDialog', [
+    async (dialogContext) => {
+        await dialogContext.context.sendActivity(`Transfer.`);
+        await dialogContext.end();
+    }
+]);
 
 // Listen for incoming requests 
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async(context) => {
         if (context.activity.type === 'message') {
-            const state = convoState.get(context);
+            const state = conversationState.get(context);
+            const dc = dialogs.createContext(context, state);
 
             const luisResults = luisRecognizer.get(context);
 
@@ -45,20 +62,18 @@ server.post('/api/messages', (req, res) => {
             const topIntent = LuisRecognizer.topIntent(luisResults, "NotFound");
             switch (topIntent) {
                 case 'Balance':                    
-                    await context.sendActivity(`You reached the Balance intent.`);
+                    await dc.begin("BalanceDialog", luisResults);
                     break;
                 case 'Transfer':                    
-                    await context.sendActivity(`You reached the Transfer intent.`);
+                    await dc.begin("TransferDialog", luisResults);
                     break;
                 case 'None':
-                    await context.sendActivity(`You reached the None intent.`);
+                    await context.sendActivity(`I dont know what you want to do. Type "make a transfer" or "get a balance".`);
                     break;
                 default:
                     break;
             }
 
-        } else {
-            await context.sendActivity(`[${context.activity.type} event detected]`);
         }
     });
 });
